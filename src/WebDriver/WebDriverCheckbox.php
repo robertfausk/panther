18ed7e158c02b5961c2a25dba045b06bf121ea25
Robert Freigang <robert.freigang@projektmotor.de>
2022-06-06 16:30:03 +0200

    Allow input type checkbox without attribute "name" // only upstream fix to be able to use it immediately

--- src/WebDriver/WebDriverCheckbox.php
+++ src/WebDriver/WebDriverCheckbox.php
@@ -37,7 +37,7 @@ class WebDriverCheckbox implements WebDriverSelectInterface
 {
     private WebDriverElement $element;
     private string $type;
-    private string $name;
+    private ?string $name;
 
     public function __construct(WebDriverElement $element)
     {
@@ -50,13 +50,9 @@ class WebDriverCheckbox implements WebDriverSelectInterface
             throw new WebDriverException('The input must be of type "checkbox" or "radio".');
         }
 
-        if (null === $name = $element->getAttribute('name')) {
-            throw new WebDriverException('The input must have a "name" attribute.');
-        }
-
         $this->element = $element;
         $this->type = $type;
-        $this->name = $name;
+        $this->name = $element->getAttribute('name');
     }
 
     public function isMultiple(): bool
@@ -231,7 +227,11 @@ class WebDriverCheckbox implements WebDriverSelectInterface
 
     private function getRelatedElements($value = null): array
     {
-        $valueSelector = null === $value ? '' : \sprintf(' and @value = %s', XPathEscaper::escapeQuotes($value));
+        if (is_null($this->name)) {
+            return [$this->element];
+        }
+
+        $valueSelector = $value ? sprintf(' and @value = %s', XPathEscaper::escapeQuotes($value)) : '';
         if (null === $formId = $this->element->getAttribute('form')) {
             $form = $this->element->findElement(WebDriverBy::xpath('ancestor::form'));
             if ('' === $formId = (string) $form->getAttribute('id')) {
